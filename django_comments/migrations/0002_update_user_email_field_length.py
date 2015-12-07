f17355b1a0f7fdee333e3ba7dc307682e2c09b28
Rick Vause <rvause@gmail.com>
2015-12-07 21:31:38 +0100

    Update comment model only if not using abstract model

--- django_comments/migrations/0002_update_user_email_field_length.py
+++ django_comments/migrations/0002_update_user_email_field_length.py
@@ -2,6 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
+
+
+is_abstract = lambda: getattr(settings, 'DJANGO_COMMENTS_ABSTRACT', False)
 
 
 class Migration(migrations.Migration):
@@ -10,12 +14,15 @@ class Migration(migrations.Migration):
         ('django_comments', '0001_initial'),
     ]
 
-    operations = [
-        migrations.AlterField(
-            model_name='comment',
-            name='user_email',
-            field=models.EmailField(
-                max_length=254, verbose_name="user's email address",
-                blank=True),
-        ),
-    ]
+    operations = []
+
+    if not is_abstract():
+        operations.append(
+            migrations.AlterField(
+                model_name='comment',
+                name='user_email',
+                field=models.EmailField(
+                    max_length=254, verbose_name="user's email address",
+                    blank=True),
+            )
+        )
